99a9711afd285f547e80edfbeab209e84120b241
S T Aswin <ktashwinbest@gmail.com>
2026-07-28 12:33:56 +0530

    Modified new_writeup.sh next_lvl.sh to handle errors

--- OverTheWire/Bandit/new_writeup.sh
+++ OverTheWire/Bandit/new_writeup.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/bash
 
+if [[ -z "$1" ]] then
+	echo "Error!,Pls Give The name for the New Writeup"
+	exit
+fi
+
 cp ~/Github_cybersecurity/ctf-writeups/ctf_write.template ./"$1.md"
 
 foot nvim "$1.md" &
--- OverTheWire/Bandit/next_lvl.sh
+++ OverTheWire/Bandit/next_lvl.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env bash
 
+if [[ -z "$1" ]] 
+then
+	echo "Pls, Provide the User name "
+	echo "Example . ./next_lvl.sh bandit0"
+	exit
+fi
+
 ssh -p 2220 "$1"@bandit.labs.overthewire.org
